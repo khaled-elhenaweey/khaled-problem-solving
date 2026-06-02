a419e1e1f093dcfdbc03d4cd53a072b3d3ee6f57
khaled El-henaweey <71648492+khaled-elhenaweey@users.noreply.github.com>
2026-06-02 12:28:31 +0300

    Update solution - CodeHub

--- 7-kyu/two-to-one/two-to-one.js
+++ 7-kyu/two-to-one/two-to-one.js
@@ -1,5 +1,3 @@
 function longest(s1, s2) {
-  const cleanStr = new Set([...s1,...s2].sort())
-  const result = [...cleanStr].join('')
-  return result
+  return [...new Set(s1 + s2)].sort().join('')
 }
